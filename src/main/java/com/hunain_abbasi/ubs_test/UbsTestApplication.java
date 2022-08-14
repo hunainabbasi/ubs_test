ee0633335ea980bed89c93a02d8db0e575dc5f56
hunainabbasi <hunainabbasi94@gmail.com>
2022-08-14 21:20:19 +0300

    Initial commit

---
+++ src/main/java/com/hunain_abbasi/ubs_test/UbsTestApplication.java
@@ -0,0 +1,13 @@
+package com.hunain_abbasi.ubs_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbsTestApplication.class, args);
+    }
+
+}
